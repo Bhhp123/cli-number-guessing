f1fb8a7b8cd238c473accb0f98b777252629e4d0
Bilawal Hussain <bilawalhussain92@gmail.com>
2024-04-24 14:45:23 +0500

    file update

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
-// 1) Computer will genrate a random number - Done
-// 2) User input for guessing number
-// 3) Compare user input with computer genrated number and show result
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-const answer = await inquirer.prompt([
-    {
-        name: "GuessedNumber",
-        type: "number",
-        message: "Please guess a number betweem 1 - 6:"
+import inquirer from 'inquirer';
+let numberToGuess = Math.floor(Math.random() * 5) + 1; // Generate a random number between 1 and 5
+let attempts = 0;
+async function guessNumber() {
+    console.log('\n\t\x1b[34m\x1b[1m Bilawal Hussain\'s Number Guessing Game \x1b[0m');
+    console.log('\t-----------------------------------------');
+    const answer = await inquirer.prompt({
+        type: 'number',
+        name: 'guess',
+        message: '\x1b[36m\x1b[1mGuess a number between 1 and 5 (or type 0 to exit):\x1b[0m',
+    });
+    if (answer.guess === 0) {
+        console.log('\t\t\x1b[33m\x1b[1mExiting game. Goodbye!\x1b[0m');
+        return;
     }
-]);
-if (answer.GuessedNumber === randomNumber) {
-    console.log("Congtratulation! you guessed right number");
+    attempts++;
+    if (answer.guess === numberToGuess) {
+        console.log(`\x1b[32m\x1b[1mCongratulations! You guessed the number in ${attempts} attempts.\x1b[0m`);
+    }
+    else if (answer.guess < numberToGuess) {
+        console.log('\t\t\x1b[33m\x1b[1mToo low! Try again.\x1b[0m');
+        await guessNumber();
+    }
+    else {
+        console.log('\t\t\x1b[31m\x1b[1mToo high! Try again.\x1b[0m');
+        await guessNumber();
+    }
 }
-else {
-    console.log("You guessed wrong number");
-}
+guessNumber();
--- index.ts
+++ index.ts
@@ -1,27 +1,36 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
+import inquirer from 'inquirer';
 
-// 1) Computer will genrate a random number - Done
+let numberToGuess = Math.floor(Math.random() * 5) +1;  // Generate a random number between 1 and 5
+let attempts = 0;
 
-// 2) User input for guessing number
+async function guessNumber() {
+  console.log('\n\t\x1b[34m\x1b[1m Bilawal Hussain\'s Number Guessing Game \x1b[0m');
+  console.log('\t-----------------------------------------');
 
-// 3) Compare user input with computer genrated number and show result
+  const answer = await inquirer.prompt({
+    type: 'number',
+    name: 'guess',
+    message: '\x1b[36m\x1b[1mGuess a number between 1 and 5 (or type 0 to exit):\x1b[0m',
+  });
 
-const randomNumber = Math.floor(Math.random()* 6 + 1);
+  if (answer.guess === 0) {
+    console.log('\t\t\x1b[33m\x1b[1mExiting game. Goodbye!\x1b[0m');
+    return;
+  }
 
-const answer = await inquirer.prompt([
-    {
-        name: "GuessedNumber",
-        type: "number",
-        message: "Please guess a number betweem 1 - 6:"
-    }
-]);
+  attempts++;
 
-if (answer.GuessedNumber === randomNumber) {
-    console.log("Congtratulation! you guessed right number");
-} else {
-  console.log("You guessed wrong number");
-    
+  if (answer.guess === numberToGuess) {
+    console.log(`\x1b[32m\x1b[1mCongratulations! You guessed the number in ${attempts} attempts.\x1b[0m`);
+  } else if (answer.guess < numberToGuess) {
+    console.log('\t\t\x1b[33m\x1b[1mToo low! Try again.\x1b[0m');
+    await guessNumber();
+  } else {
+    console.log('\t\t\x1b[31m\x1b[1mToo high! Try again.\x1b[0m');
+    await guessNumber();
+  }
 }
 
+guessNumber();
 
